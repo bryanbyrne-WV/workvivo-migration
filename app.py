2fdafc4effc5169e877e3d763def2fc8da73fc02
bryanbyrne-WV <bryan.byrne@workvivo.com>
2025-11-21 11:22:21 +0000

    Update app.py

--- app.py
+++ app.py
@@ -211,7 +211,7 @@ with st.sidebar:
 
     # CONFIGURATION
     st.markdown(
-        """<a class="sidebar-link" href="#_config">⚙Configuration</a>""",
+        """<a class="sidebar-link" href="#_config">Configuration</a>""",
         unsafe_allow_html=True
     )
 
