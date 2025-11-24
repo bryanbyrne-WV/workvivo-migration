ab42de6c792b40e4ee9c017210113a2555941603
bryanbyrne-WV <bryan.byrne@workvivo.com>
2025-11-24 11:14:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -506,7 +506,7 @@ if "config_saved" not in st.session_state:
 
         # CONTINUE BUTTON (purple)
         st.markdown('<div class="purple-btn">', unsafe_allow_html=True)
-        if st.button("Continue"):
+        if st.button("➡ Continue"):
             st.rerun()
         st.markdown('</div>', unsafe_allow_html=True)
 
