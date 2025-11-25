6fbe2ef149e6d5ab7e3370d4e6cd5d4b625299ed
bryanbyrne-WV <bryan.byrne@workvivo.com>
2025-11-25 17:23:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ if not st.session_state.authenticated:
     # Centered Workvivo logo above login form
     st.markdown("""
     <div style="text-align:center; margin-top:40px; margin-bottom:10px;">
-        <img src="https://www.festivalofwork.com/media/Workvivo%20(1).png"
+        <img src="https://d3lkrqe5vfp7un.cloudfront.net/images/WorkVivo-1.png"
              style="height:90px;">
     </div>
     """, unsafe_allow_html=True)
