6bcfe84e854f2bf028fe2ca7fddcba9378ee30d4
bryanbyrne-WV <bryan.byrne@workvivo.com>
2025-12-02 09:28:04 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2623,16 +2623,21 @@ elif st.session_state.page == "summary":
         else "Migration Completed Successfully"
     )
     
-    # NEW — Subtitle logic
     title_sub = (
         ""
         if is_cancelled
         else "All migration tasks completed successfully."
     )
     
-    # Green for success, red for cancelled
     title_color = "#CC0000" if is_cancelled else "#4CAF50"
     
+    # DISPLAY HEADER
+    st.markdown(f"""
+        <h2 style="color:{title_color}; font-weight:800; margin-bottom:4px;">
+            {title_text}
+        </h2>
+    """, unsafe_allow_html=True)
+        
     
     # -------------------------------------------------------
     # DISPLAY HEADER
